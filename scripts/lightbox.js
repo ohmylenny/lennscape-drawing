9a5be1d6b162542aac94f429ebb52e97e27f2bbe
Lenny <161146624+ohmylenny@users.noreply.github.com>
2025-06-13 13:38:12 +0700

    Update lightbox.js

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,19 +1,115 @@
 const artDescriptions = {
   'Fin.png': {
   title: 'This Is Fin',
-  description: 'A sweet, chill moment under the purple sky—what could be sweeter than Fin?'
+  description: 'Gwarrrkkk, has amazing cutie voice. Fin made from love and fun.'
 },
 'Vanx.png': {
   title: 'VannnX',
-  description: 'An abstract flame-skull fusion in cosmic firelight.'
+  description: 'Indo-Nads, Well known as infographic maker.'
 },
 "Let's.png": {
   title: 'Leys BOBR',
-  description: 'BOBR LEADER, you can learn Polish from Him.'
+  description: 'BOBR LEADER, you can learn Polish from Him. KURWAAAA. I always waiting for his tweet everyday.'
 },
 'Evmox.png': {
   title: 'Evm0X',
   description: 'Roaming everywhere, find him for a secret story.'
+}
+  'John_W_Rich_Kid.png': {
+  title: 'John W Rich Kid',
+  description: 'Smartest Monad community team.'
+}
+'HOS.png': {
+  title: 'House OF Sharky',
+  description: 'A deep sea vibes, all sharks, Ghos. Do not make mom mad!.'
+}
+'HOS2.png': {
+  title: 'House OF Sharky',
+  description: 'Same, place for all sharks. Knock the door to meet Sharkdog, the best mommy.'
+}
+'Potato_.png': {
+  title: 'Potato13 (Ziemniak)',
+  description: 'Humble Polish-Nad, BOBR familia, he is everywhere. Super kind!'
+}
+'Nini.png': {
+  title: 'Sailornini',
+  description: 'Cool, little bit mysterious, but kindhearted inside. a KIND community team members.'
+}
+'Bruno.png': {
+  title: 'Bruno',
+  description: 'Humble, kindhearted, supportive. A gym bros too. Portuguese-Nad'
+}
+'Zoe.png': {
+  title: 'Zo',
+  description: 'Call him Habibi, has a lot of oil in his backyard, he loves camel. Indo-Nads'
+}
+'Ghooolyache.png': {
+  title: 'Ghoolyache',
+  description: 'Great crowdmaker, simp too much. He loves Asian girl. Professional gamers at that time'
+}
+'Urban_Kryptoo.png': {
+  title: 'Urban Kryptoo',
+  description: 'Supportive, he is everywhere.'
+}
+'Sheldon.png': {
+  title: 'Sheldon',
+  description: 'Monad Artist. Talented person with his unique style. He is a good boy, but badboi aura covered him so much. Love his Moaister draws!'
+}
+'Chikuuu.png': {
+  title: 'Chikuuu',
+  description: 'He is everywhere. Another supportive Nads! He taking care of many community members. I learn to play gartic from him.'
+}
+'Sosuke.png': {
+  title: 'Sosuke',
+  description: 'This is my first sister. She is diligent, smart, and really cute. She loves reading so much that the library feels like her second home. I have learned a lot from her. '
+}
+'Yassy.png': {
+  title: 'Yassy',
+  description: 'She is well known as a Monad clay art master. So talented, she crafts each piece with a heartfelt touch that makes them feel alive. Beyond her creativity, she is also incredibly humble. Ily my sis!'
+}
+'Papa_Foodie.png': {
+  title: 'Papa Foodie',
+  description: 'He is a crazy monad chef. You can visit him to see how he cook masterpiece of meals. I guess he know best dishes to make in every season. Beside cooking, he loves to play a game. You can join a joyful game events with him. He is so warm, everybody love papa.'
+}
+'Zahid.png': {
+  title: 'Zahid',
+  description: 'He is a funny guy, humble and kind (sometimes). If he shows up in your post, just say "OK" and you will be fine. You will recognize his memes from that liquify style. He is a good shitposter too. I am really glad to know him until now.'
+}
+'Zamin.png': {
+  title: 'Zamin Baldnad',
+  description: 'He is a funny guy, and sometimes Nads think he is Zahid’s twin. But he is different, especially with his hairstyle. His keyboard is broken now and the caps lock is stuck. You can find him in Monad, have a chat and you might feel like you just wasted a few minutes doing it.'
+}
+'Seong.png': {
+  title: 'Seong',
+  description: 'Maybe some of you already know. She is talented artist, kind and humble person. She loves to doing gym exercise, good for your healthy figure to follow. Look how cute she is at this mirror selfie!'
+}
+'Halosunny.png': {
+  title: 'Halosunny',
+  description: 'She is talented, active, and humble. She often draws gnad stickers and they are all very cute. I really love the one she made for me, I look like I am sleeping beauty, lol. Look at what she is doing in her room. Maybe she is not just watching the moonlight, but also thinking about a wonderful life.'
+}
+'Maverick_.png': {
+  title: 'Maverick',
+  description: 'One of my favorite singers in Monad. So talented, bringing songs to life with his unique style. He’s kind and humble too. Because of him, I started learning to draw anime. Can you believe it? He sings and performs Jutsu too!'
+}
+'Jiraiya.png': {
+  title: 'Jiraiya',
+  description: 'One of the great music makers in Monad. He’s created many masterpieces in his signature Mo-Fi style. Humble and easy to talk to, you can always reach out to share ideas or even make music together. Now look, he’s writing a book about creating Mo-Fi music. Curious to see what’s inside?'
+}
+'Potato_King.png': {
+  title: 'Potato King',
+  description: 'A humble and kind artist from the Monad community. He truly loves to draw. Look at him under the starry night. Seems like a cup of coffee lifted his mood to write some poems.'
+}
+'Ipong.png': {
+  title: 'Ipongresu',
+  description: 'The one humble guy who usually lived in gen3. The creator of Monbuds pfp collection. He is both fun and naughty guy lmao. Sometimes he cooked me but that is the way we loved each other. See, he looks so mysterious. What he will do with the wooden stick?'
+}
+'Karim_Madridista.png': {
+  title: 'Madridistaaa (Karim)',
+  description: 'Another talented member of the Monad community. He loves to draw, so humble, and has a great sense of humor. Sometimes he blends memes with his unique drawing style. He’s a fun guy to hang out with and full of potential. Just look at what he’s working on, with so many sketches waiting to be finished.'
+}
+'Chocobear.png': {
+  title: 'Chocobear',
+  description: 'A humble guy and a true gamer. He gets super excited whenever there’s a gaming event in the community. You can challenge him for a match or ask for tips on leveling up your game. Another amazing Nads. Look at him now, heading out on his scooter to find food after 23 hours of straight gaming lmao.'
 }
 
   // Add more entries as needed, matching the image filenames exactly
